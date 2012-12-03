e3c694fa8701902234836d017da615a9f4e492a1
Nitin Punjabi <nptoronto@yahoo.com>
2012-12-03 09:51:04 +0100

    Specify a connection protocol

--- project.clj
+++ project.clj
@@ -1,3 +1,9 @@
-(defproject clj-fix "1.0.0-SNAPSHOT"
+(defproject clj-fix "0.1"
   :description "FIXME: write description"
-  :dependencies [[org.clojure/clojure "1.3.0"]])
+  :dependencies [[org.clojure/clojure "1.4.0"]
+                 [clj-time "0.4.4"]
+                 [aleph "0.3.0-beta8"] 
+                 [lamina "0.5.0-beta8"]
+                 [gloss "0.2.2-beta3"]]
+  :native-path "/usr/local/lib"
+  :main clj-fix.core)
--- src/clj_fix/connection/protocol.clj
+++ src/clj_fix/connection/protocol.clj
@@ -1,25 +1,11 @@
-(ns clj-fix.core
-  (:use clj-fix.connection.protocol)
-  (:require (clojure [string :as s])
-            (lamina [core :as l])
-            (aleph [tcp :as a])
-            (gloss [core :as g])))
+(ns clj-fix.connection.protocol)
 
-
-(defrecord FixConn [id]
-  Connection
+(defprotocol Connection
   (connect [id])
-
   (logon [id msg-handler heartbeat-interval])
-
   (buy [id size instrument-symbol price & additional-params])
-
   (sell [id size instrument-symbol price & additional-params])
-
   (cancel [id order])
-
   (cancel-replace [id order & additional-params])
-
   (order-status [id order])
-
   (logout [id reason]))
