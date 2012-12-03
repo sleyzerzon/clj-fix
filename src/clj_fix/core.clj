814bccea70d682957ae70f310a4c16707efec13b
Nitin Punjabi <nptoronto@yahoo.com>
2012-12-03 09:42:20 +0100

    First commit

---
+++ src/clj_fix/core.clj
@@ -0,0 +1,1 @@
+(ns clj-fix.core)
---
+++ test/clj_fix/test/core.clj
@@ -0,0 +1,6 @@
+(ns clj-fix.test.core
+  (:use [clj-fix.core])
+  (:use [clojure.test]))
+
+(deftest replace-me ;; FIXME: write
+  (is false "No tests have been written."))
