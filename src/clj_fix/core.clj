572642b65a49039d6f2056d4023efd426903e4ae
Nitin Punjabi <nptoronto@yahoo.com>
2012-12-06 09:33:13 +0100

    Implement message handling

--- src/clj_fix/core.clj
+++ src/clj_fix/core.clj
@@ -7,6 +7,9 @@
             (gloss [core :as g])
             (clj-time [core :as t] [format :as f])))
 
+(def ^:const msg-delimiter #"10=\d{3}\u0001")
+(def ^:const msg-identifier #"(?<=10=\d{3}\u0001)")
+
 (defrecord Conn [venue host port sender target channel in-seq-num
                 out-seq-num msg-handler next-msg msg-fragment translate?])
 
@@ -43,19 +46,70 @@
           (println (.getMessage e)))))
     (error "Channel already open.")))
 
+(defn segment-msg
+  ; Takes a TCP segment and splits it into individual FIX messages. If the last
+  ; message in the collection is complete, appends a blank
+  [msg]
+  (let [segments (s/split msg msg-identifier)]
+    (if (re-find msg-delimiter (peek segments))
+      (conj segments "")
+      segments)))
+
 (defn gen-msg-sig [session]
   (let [{:keys [out-seq-num sender target]} session]
     {:msg-seq-num (swap! out-seq-num inc)
      :sender-comp-id sender
      :target-comp-id target
-     :transact-time (timestamp)}))
+     :sending-time (timestamp)}))
 
 (defn send-msg [session msg-type msg-body]
-  (let [msg (into {:msg-type msg-type} [(gen-msg-sig session) msg-body])]
-    (encode-msg (:venue session) msg)))
+  (let [msg (into {:msg-type msg-type} [(gen-msg-sig session) msg-body])
+        encoded-msg (encode-msg (:venue session) msg)]
+    (println "clj-fix sending" encoded-msg)
+    (l/enqueue (get-channel session) encoded-msg)))
+
+(defn update-next-msg [old-msg new-msg] new-msg)
+
+(defn update-user [session payload]
+  (send (:next-msg session) update-next-msg payload))
+
+(defn transmit-heartbeat
+  ([session]
+    (send-msg session :heartbeat {}))
+  ([session test-request-id]
+    (send-msg session :heartbeat {:test-request-id test-request-id})))
 
 (defn gen-msg-handler [id]
-  (fn [msg] (println msg)))
+  (fn [msg]
+    ; Need to:
+    ; 1) Update inbound sequence number.
+    ; 2) Get more useful information for unknown order message types.
+    (let [session (get-session id)
+          msg-fragment (:msg-fragment session)
+          segments (segment-msg msg)
+          msgs (assoc segments 0 (s/join "" [@msg-fragment (first segments)]))]
+
+      (if-let [msgs-to-proc (butlast msgs)]
+        (doseq [m msgs-to-proc]
+          (let [venue (:venue session)
+                msg-type (get-msg-type venue m)]
+            (println "clj-fix received" m)
+            (case msg-type
+              :logon (update-user session {:msg-type msg-type :accepted true
+                                           :sender-comp-id (:sender-comp-id
+                                                             (decode-msg venue
+                                                              msg-type msg))})
+              :heartbeat (transmit-heartbeat session)
+              :test-request (transmit-heartbeat session (:test-request-id
+                              (decode-msg venue msg-type msg)))
+              :reject (println "SESSION REJECT")
+              :execution-report (println "EXECUTION REPORT")
+              :order-cancel-reject (println "ORDER CANCEL REJECT")
+              :indication-of-interest (println "INDICATION OF INTEREST")
+              :logout (println "LOGOUT")
+              :resend-request (println "RESEND REQUEST")
+              :seq-reset (println "SEQUENCE RESET")
+              (print "Unknown message type"))))))))
 
 (defn connect 
   ([id translate-returning-msgs]
@@ -70,7 +124,13 @@
   Connection
   (logon [id msg-handler heartbeat-interval reset-seq-num
           translate-returning-msgs]
+    ; Two things need to be added here:
+    ; 1) Registering the user's msg handler
+    ; 2) In the event the user requests a sequence reset, do so.
     (let [session (get-session id)]
+      (if (not (open-channel? session))
+        (connect id translate-returning-msgs))
+      (add-watch (:next-msg session) :user-callback msg-handler)
       (send-msg session :logon {:heartbeat-interval 0
                                 :reset-seq-num reset-seq-num
                                 :encrypt-method :none})))
@@ -96,7 +156,7 @@
         (do
           (swap! sessions assoc id (Conn. venue host port sender target
                                           (atom nil) (atom 0) (atom 0)
-                                          (atom nil) (agent "") (atom "")
+                                          (atom nil) (agent {}) (atom "")
                                           (atom nil)))
         (FixConn. id))
       (error (str "Session " id " already exists. Please close it first."))))
